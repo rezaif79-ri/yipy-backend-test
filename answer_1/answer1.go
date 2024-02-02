558506e35622bc6749de699d52ebd38508043eb8
Reza IF <rezaif79.ri@gmail.com>
2024-02-02 16:39:58 +0700

    feat: add answer1

---
+++ answer_1/answer1.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+
+	fmt.Print("Input n: ")
+	var n int
+	if scanner.Scan() {
+		input, err := strconv.Atoi(scanner.Text())
+		if err != nil {
+			fmt.Println("error parsing input: ", err)
+			return
+		}
+		n = input
+	}
+
+	snum := GenerateSNum(n)
+
+	for i := 1; i <= n; i++ {
+		fmt.Println(GetFormatedSNum(i, snum))
+	}
+}
+
+// GenerateSNum return slice string of number in ordered postion
+// Takes n argument as how much number count + 3 (spaces needed for ** symbol and last number)
+func GenerateSNum(n int) []string {
+	var res []string
+	for i := 1; i <= n+3; i++ {
+		res = append(res, fmt.Sprint(i))
+	}
+	return res
+}
+
+// GetFormatedSNum return slice of string formatted with question 1 requirement format (n * * n)
+func GetFormatedSNum(n int, snum []string) []string {
+	var res []string
+	if len(snum) > n+2 {
+		res = append(res, snum...)
+		res[n] = "*"
+		res[n+1] = "*"
+		return res
+	}
+	return nil
+}
